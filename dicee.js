cd9be51d146ee138d8f6a1aca0641387d584be39
sakshamnagar <142980837+sakshamnagar@users.noreply.github.com>
2023-12-20 22:04:51 +0530

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -15,20 +15,3 @@ btn.addEventListener("click", function onclick(event){
     document.getElementById("btn").innerHTML="Play Again?";
 })
 
-function login() {
-    var a = document.getElementById('loginform');
-    var b = document.getElementById('registerform');
-    if (a.style.display === 'none') {
-        a.style.display = 'block';
-        b.style.display = 'none';
-    }
-}
-
-function register() {
-    var b = document.getElementById('registerform');
-    var a = document.getElementById('loginform');
-    if (b.style.display === 'none') {
-        b.style.display = 'block';
-        a.style.display = 'none';
-    }
-}
